d89ef6eba0dea3713a7aa50c57da70a24814348e
IT-Administrator Magazin <info@it-administrator.de>
2026-07-02 16:02:05 +0200

    Update listing-iis-dsc-pruefen.ps1

--- iis-dsc/listing-iis-dsc-pruefen.ps1
+++ iis-dsc/listing-iis-dsc-pruefen.ps1
@@ -1,1 +1,21 @@
-
+Configuration MeineWebsite
+{
+  Node ("s1.contoso.int", "s2.contoso.int")
+  {
+  #IIS-Installation sicherstellen
+WindowsFeature IIS
+{
+Ensure = "Present"
+Name = "Web-Server"
+  }
+  #Existenz Webdateien sicherstellen
+File Beispieldatei
+  {
+  Ensure = "Present"
+  Type = "Directory"
+  Recurse= $true
+  SourcePath = "\\dc01\Daten"
+  Destinationpath = "C:\inetpub\wwwroot"
+  }
+  }
+}
